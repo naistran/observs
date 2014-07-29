d3258b0d9918cf438c594e58945de4b306de0d3a
Nathan Tran <ntran013@gmail.com>
2014-07-29 13:52:28 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# observs
+
+`observ` modules.
---
+++ index.js
@@ -0,0 +1,11 @@
+
+/**
+ * Module dependencies.
+ */
+
+module.exports = {
+  array: require('observ-array'),
+  struct: require('observ-struct'),
+  hash: require('observ-varhash'),
+  value: require('observ')
+};
